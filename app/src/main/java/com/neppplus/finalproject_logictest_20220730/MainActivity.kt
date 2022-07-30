8305358b4fcb353058073c2dc6973f0a9ce04768
넵플러스 <nepp.plus@gmail.com>
2022-07-30 11:24:49 +0900

    랜덤 숫자 뽑는 반복과, 텍스트뷰에 반영하는 반복 분리   - 숫자를 정리하고 나서 표시하기 위함

--- app/src/main/java/com/neppplus/finalproject_logictest_20220730/MainActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_logictest_20220730/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    val mLottoNumTextViews = ArrayList<TextView>()
+    val mLottoNumbers = ArrayList<Int>() // 당첨번호 6개를 담을 목록
+    val mLottoNumTextViews = ArrayList<TextView>() // 표시용 텍스트 6개를 담을 목록
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +27,27 @@ class MainActivity : BaseActivity() {
 
         binding.btnCheckLottoNum.setOnClickListener {
 
+//            랜덤 숫자 6개를 채우기 전에, 기존의 숫자 목록은 비우고 채우자.
+            mLottoNumbers.clear()
+
+//            랜덤 숫자 (1~45) 6개를 먼저 생성 > 목록에 담아두자. (숫자 6개를 담을 목록도 필요함)
+//            랜덤 숫자를 6회 반복 생성
+            
+            for (i in 0..5) {
+//                단순 6회 반복 for문
+                
+//                1~45 중의 숫자 하나 랜덤 추출
+                val randomNum = (1..45).random()
+                
+//                당첨번호 목록으로 추가 (0~5번칸  채움)
+                mLottoNumbers.add( randomNum )
+                
+            }
+            
+
+//            담아둔 숫자를 정리.
+
+
 //            텍스트뷰 6개를 반복적으로 처리 : 반복문 이용
 //            txt01, txt02, txt03... 등등 텍스트뷰 목록을 만들고 하나씩 꺼내다 쓰자.
 //            for 활용 > 목록에서 하나씩 꺼내주는 역할.
@@ -35,11 +57,14 @@ class MainActivity : BaseActivity() {
 //                lottoNumTxt 변수에는, txtLottoNum01~06이 순서대로 담기면서 반복 실행됨.
 //                + index 변수에는 지금이 몇바퀴째인지 (0,1,2,..,5) 의 숫자가 담겨있게 됨.
 
-//                1~45의 랜덤 숫자를 뽑자.
 
-                val randomNum = (1..45).random()
+//                랜덤을 뽑자마자 > 텍스트뷰에 배치하면, 작은 수 ~ 큰 수 대로 정리되지 않음.
+//                별도의 for문을 통해서, 6개의 랜덤 숫자를 만들고 > 정리 한 다음 에 > 텍스트뷰에 반영만 따로 반복.
 
-                lottoNumTxt.text = randomNum.toString()
+//                당첨번호 6개를 순서대로 뽑아서 표시
+//                반복 순서에 맞는 위치의 랜덤값을 찾아서 텍스트뷰에 순서대로 반영
+
+                lottoNumTxt.text = mLottoNumbers[index].toString()
 
             }
 
