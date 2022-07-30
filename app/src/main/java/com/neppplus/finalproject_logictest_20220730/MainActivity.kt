84464d874c174942bc754016b99adc69a4b08203
넵플러스 <nepp.plus@gmail.com>
2022-07-30 10:11:32 +0900

    데이터 바인딩 세팅 완료

--- app/src/main/java/com/neppplus/finalproject_logictest_20220730/MainActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_logictest_20220730/MainActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.finalproject_logictest_20220730
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_logictest_20220730.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
